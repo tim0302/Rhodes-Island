ac3131c1375dc6eb7300e62e6fb9a6ce2fd98c2c
102419310 <54136440+tim0302@users.noreply.github.com>
2022-11-09 22:58:15 +1100

    Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import {
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from '@aws-amplify/ui-react';
 
-function App() {
+function App(signOut) {
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <div>
+        <ul className='menu-bar'>
+          <li>
+            <a className='menu-bar-item' href='.'>
+              Rhodes Island
+            </a>
+          </li>
+          <li>
+            <a className='menu-bar-item' href='.'>
+              Operator Profile
+            </a>
+          </li>
+          <li>
+            <a className='menu-bar-item' href='.'>
+              Bridge - Exploration of Arts
+            </a>
+          </li>
+          <li>
+            <a className='menu-bar-item' href='.'>
+              Voice From The Deep
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className='main'>
+        <div className='title'>
+          <h1>Welcome Aboard!</h1>
+        </div>
+        <div className='title-content'>
+          <p>This is just some random place holder of the content</p>
+        </div>
+      </div>
+      <View className='App'>
+        <Card>
+          <Heading level={1}>We now have Auth!</Heading>
+        </Card>
+        <Button onClick={signOut}>Sign Out</Button>
+      </View>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
